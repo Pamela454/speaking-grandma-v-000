54a68801428a9f89da42939b2894feb9b82761f7
Pamela Frieze <pfrieze@gmail.com>
2017-09-19 04:40:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.upcase == phrase 
+  if phrase.upcase == phrase
   return "HUH? SPEAK UP, SONNY!"
+else
   return "NO, NOT SINCE 1938!"
   if phrase == "I LOVE YOU GRANDMA" then return "I LOVE YOU TOO PUMPKIN!"
 end
