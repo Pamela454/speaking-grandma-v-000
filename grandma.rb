94be8bfebb1352b41fad438a2e0743449ecac60f
Pamela Frieze <pfrieze@gmail.com>
2017-09-19 04:21:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   phrase.uppercase?
+  if false then return "HUH? SPEAK UP, SONNY!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
